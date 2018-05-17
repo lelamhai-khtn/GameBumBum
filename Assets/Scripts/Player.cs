0ccf149a818d3ce0100b96ad5cb6e71ac0b3da92
lelamhai <leelamhair@gmail.com>
2018-05-18 06:56:42 +0700

    init game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public Vector3 target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            target = hit.point;
+
+            if(hit.transform.tag.Equals("Enemy"))
+            {
+                Debug.Log(11111);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        transform.LookAt(target);
+	}
+
+}
